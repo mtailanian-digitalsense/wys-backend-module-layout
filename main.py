fbdf808ead39d2b2d3068f74e4a6309e422e63e9
Rodrigo Hernández <rjhernandr@gmail.com>
2020-09-04 10:04:33 -0400

    Added: Construction of input data for the layout engine in POST Endpoint.

--- main.py
+++ main.py
@@ -28,18 +28,18 @@ DB_PORT = os.getenv('DB_PORT', '3307')
 DB_SCHEMA = os.getenv('DB_SCHEMA', 'wys')
 APP_HOST = os.getenv('APP_HOST', '127.0.0.1')
 APP_PORT = os.getenv('APP_PORT', 5006)
+
+#Buildings module info
 BUILDINGS_MODULE_HOST = os.getenv('BUILDINGS_MODULE_HOST', '127.0.0.1')
 BUILDINGS_MODULE_PORT = os.getenv('BUILDINGS_MODULE_PORT', 5004)
-BUILDINGS_MODULE_API_LOCS = os.getenv('BUILDINGS_MODULE_API_LOCS_GET', '/api/buildings/locations/')
+BUILDINGS_MODULE_API = os.getenv('BUILDINGS_MODULE_API', '/api/buildings/')
+BUILDINGS_URL = f"http://{BUILDINGS_MODULE_HOST}:{BUILDINGS_MODULE_PORT}"
+
+#Spaces module info
 SPACES_MODULE_HOST = os.getenv('SPACES_MODULE_IP', '127.0.0.1')
 SPACES_MODULE_PORT = os.getenv('SPACES_MODULE_PORT', 5002)
-SPACES_MODULE_API_CREATE = os.getenv('SPACES_MODULE_API_CREATE', '/api/spaces/create')
-PROJECTS_MODULE_HOST = os.getenv('PROJECTS_MODULE_HOST', '127.0.0.1')
-PROJECTS_MODULE_PORT = os.getenv('PROJECTS_MODULE_PORT', 5000)
-PROJECTS_MODULE_API = os.getenv('PROJECTS_MODULE_API', '/api/projects/')
-PROJECTS_URL = f"http://{PROJECTS_MODULE_HOST}:{PROJECTS_MODULE_PORT}"
+SPACES_MODULE_API = os.getenv('SPACES_MODULE_API', '/api/spaces/')
 SPACES_URL = f"http://{SPACES_MODULE_HOST}:{SPACES_MODULE_PORT}"
-BUILDINGS_URL = f"http://{BUILDINGS_MODULE_HOST}:{BUILDINGS_MODULE_PORT}"
 
 app = Flask(__name__)
 CORS(app)
@@ -197,6 +197,231 @@ class LayoutZone(db.Model):
         return jsonify(self.to_dict())
 
 db.create_all() # Create all tables
+
+# Swagger Config
+
+SWAGGER_URL = '/api/layouts/docs/'
+API_URL = '/api/layouts/spec'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "WYS API - Layout Service"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+def get_space_by_id(space_id, token):
+    headers = {'Authorization': token}
+    api_url = SPACES_URL + SPACES_MODULE_API + str(space_id)
+    rv = requests.get(api_url, headers=headers)
+    if rv.status_code == 200:
+        return json.loads(rv.text)
+    elif rv.status_code == 500:
+      raise Exception("Cannot connect to the spaces module")
+    return None
+
+def get_floor_polygons_by_ids(building_id, floor_id, token):
+    headers = {'Authorization': token}
+    api_url = BUILDINGS_URL + BUILDINGS_MODULE_API + str(building_id) + '/floors/'+ str(floor_id) + '/polygons'
+    rv = requests.get(api_url, headers=headers)
+    if rv.status_code == 200:
+        return json.loads(rv.text)
+    elif rv.status_code == 500:
+      raise Exception("Cannot connect to the buildings module")
+    return None
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+
+        bearer_token = request.headers.get('Authorization', None)
+        try:
+            token = bearer_token.split(" ")[1]
+        except Exception as ierr:
+            app.logger.error(ierr)
+            return jsonify({'message': 'a valid bearer token is missing'}), 500
+
+        if not token:
+            app.logger.debug("token_required")
+            return jsonify({'message': 'a valid token is missing'})
+
+        app.logger.debug("Token: " + token)
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'],
+                              algorithms=['RS256'], audience="1")
+            user_id: int = data['user_id']
+            request.environ['user_id'] = user_id
+        except Exception as err:
+            return jsonify({'message': 'token is invalid', 'error': err})
+        except KeyError as kerr:
+            return jsonify({'message': 'Can\'t find user_id in token', 'error': kerr})
+
+        return f(*args, **kwargs)
+
+    return decorator
+
+@app.route("/api/layouts/spec", methods=['GET'])
+@token_required
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "WYS Layout API Service"
+    swag['tags'] = [{
+        "name": "Layouts",
+        "description": "Methods to configure layouts"
+    }]
+    return jsonify(swag)
+
+@app.route("/api/layout/<project_id>", methods=['POST'])
+@token_required
+def generate_layout(project_id):
+    """
+        Return the information necessary to find floors
+        ---
+        consumes:
+        - "application/json"
+        tags:
+        - Layouts
+        produces:
+        - application/json
+        parameters:
+        - in: path
+          name: project_id
+          type: integer
+          description: Project ID
+        - in: "body"
+          name: "body"
+          required:
+          - selected_floor
+          - workspaces
+          properties:
+            selected_floor:
+                type: object
+                description: Data of the floor selected by the user.
+                properties:
+                    id:
+                        type: integer
+                        description: Unique ID of each building floor.
+                    wys_id:
+                        type: string
+                        description: Unique internal ID of each building floor.
+                    rent_value:
+                        type: number
+                        format: float
+                        description: Rent value of the floor
+                    m2:
+                        type: number
+                        format: float
+                        description: Square meter value of the floor.
+                    elevators_number:
+                        type: integer
+                        description: Numbers of elevators in the floor.
+                    image_link:
+                        type: string
+                        description: Link of the floor image.
+            workspaces:
+                type: array
+                items:
+                    type: object
+                    properties:
+                        id:
+                            type: integer
+                            description: Unique ID of each space.
+                        quantity:
+                            type: integer
+                            description: Quantity of the space
+                        name:
+                            type: string
+                            description: Name of the space
+                        model_2d:
+                            type: string
+                            description: Base64 file
+                        model_3d:
+                            type: string
+                            description: Base64 file
+                        height:
+                            type: number
+                            description: Height of the space
+                        width:
+                            type: number
+                            description: width of the space
+                        active:
+                            type: boolean
+                            description: indicate if this space is active
+                        regular:
+                            type: boolean
+                            description: indicate if this space is a regular space
+                        up_gap:
+                            type: number
+                            description: up padding
+                        down_gap:
+                            type: number
+                            description: down padding
+                        left_gap:
+                            type: number
+                            description: left padding
+                        right_gap:
+                            type: number
+                            description: right padding
+                        subcategory_id:
+                            type: integer
+                            description: subcategory Id
+                        points:
+                            type: array
+                            items:
+                                type: object
+                                properties:
+                                    x:
+                                        type: number
+                                        format: float
+                                        description: X coordinate of the vertex/point.
+                                    y:
+                                        type: number
+                                        format: float
+                                        description: Y coordinate of the vertex/point.
+        responses:
+            200:
+                description: Return Countries, Zones, Regions
+            500:
+                description: Internal Error Server
+    """
+    try:
+        params = {'selected_floor', 'workspaces'}
+        if request.json.keys() != params:
+            return "A required field is missing in the body", 400
+
+        floor = request.json['selected_floor']
+        floor_params = {'id', 'wys_id','rent_value','m2','elevators_number','image_link','active','building_id'}
+        if floor.keys() != floor_params:
+             return "A floor data field is missing in the body", 400
+
+        workspaces = request.json['workspaces']
+
+        if len(workspaces) == 0:
+            return "No spaces were entered in the body.", 400
+        workspace_params = {'id','quantity','name','model_2d','model_3d','height','width','active','regular','up_gap','down_gap','left_gap','right_gap','subcategory_id','points'}
+        for workspace in workspaces:
+            if workspace.keys() != workspace_params:
+             return "A space data field is missing in the body", 400
+
+        token = request.headers.get('Authorization', None)
+        floor_polygons = get_floor_polygons_by_ids(floor['building_id'], floor['id'], token)
+        if floor_polygons is None:
+            return "The floor doesn't exist or not have a polygons.", 404
+        floor['polygons'] = floor_polygons
+        layout_data = {'selected_floor': floor, 'workspaces': workspaces}
+        
+        return jsonify(layout_data), 201
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return f'Error saving data: {e}', 500
+    except Exception as exp:
+        msg = f"Error: mesg ->{exp}"
+        app.logger.error(msg)
+        return msg, 500
+
 
 if __name__ == '__main__':
   app.run(host = APP_HOST, port = APP_PORT, debug = True)
