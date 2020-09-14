e263795eb7e0ccb7e2d7ce8973dfdac4a0beef65
Rodrigo Hernández <rjhernandr@gmail.com>
2020-09-14 11:54:12 -0300

    Changed: SL Parameters

--- Layout_App/SmartLayout.py
+++ Layout_App/SmartLayout.py
@@ -94,7 +94,7 @@ def makePos(planta, minx, miny, maxx, maxy):
             return mod
 
 
-def Smart_Layout(dictionary, POP_SIZE=50, GENERATIONS=50):
+def Smart_Layout(dictionary, POP_SIZE=75, GENERATIONS=75):
     outline, holes, areas, input_list = get_input(dictionary)
 
     voids = []
