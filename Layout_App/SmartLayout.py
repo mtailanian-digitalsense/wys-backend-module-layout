ef8f42bdee466e4979dbca961653e40154dea7e8
Rodrigo Hernández <rjhernandr@gmail.com>
2020-10-18 14:05:23 -0300

    Fixed: Problem on initial position of modules

--- Layout_App/SmartLayout.py
+++ Layout_App/SmartLayout.py
@@ -80,7 +80,7 @@ def makePos(planta, in_list, zones):
     global makeposcnt
     global curr_bx
     in_cnt = 0
-    zone = []
+    z = []
 
     mod = Module(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
@@ -110,6 +110,7 @@ def makePos(planta, in_list, zones):
         zone = z[0]
         minx, miny, maxx, maxy = zone.bounds
     else:
+        zone = []
         minx, miny, maxx, maxy = planta.bounds
     
     #print(round(time.time() - start_time, 2), len(curr_bx), mod.name)
@@ -117,9 +118,12 @@ def makePos(planta, in_list, zones):
         p = Point(round(random.uniform(minx, maxx), 1), round(random.uniform(miny, maxy), 1))
         b = box(p.x - mod.width / 2, p.y - mod.height / 2, p.x + mod.width / 2, p.y + mod.height / 2)
 
-        condition1 = planta.contains(b)
+        if zone:
+            condition1 = zone.contains(b)
+        else:
+            condition1 = planta.contains(b)
 
-        if (time.time() - make_time) > 0.5 and condition1:
+        if (time.time() - make_time) > 0.05 and condition1:
             mod.x, mod.y = p.x, p.y
             curr_bx.append(b)
             # for cb in curr_bx:
@@ -208,8 +212,8 @@ def make_zones(planta, shafts, core, entrances, cat_area):
         dist_x = abs(p_maxx - p_minx)
         dist_y = abs(p_maxy - p_miny)
         if(dist_x >= dist_y):
-            zpt = box(p_minx, p_miny, p_minx + dist_x*0.3, p_miny + dist_y*0.7).intersection(planta)
-            zpt2 = box(p_maxx - dist_x*0.3, p_miny, p_maxx, p_miny + dist_y*0.7).intersection(planta)
+            zpt = box(p_minx, p_miny, p_minx + dist_x*0.3, p_miny + dist_y*0.8).intersection(planta)
+            zpt2 = box(p_maxx - dist_x*0.3, p_miny, p_maxx, p_miny + dist_y*0.8).intersection(planta)
         elif(dist_x < dist_y):
             zpt = box(p_minx, p_miny, p_minx + dist_x*0.7, p_miny + dist_y*0.3).intersection(planta)
             #zpt2 = box(p_minx, p_miny, p_minx + dist_x*0.3, p_miny + dist_y*0.7).intersection(planta)
@@ -317,18 +321,18 @@ def Smart_Layout(dictionary, POP_SIZE, GENERATIONS, viz=False, viz_period=10):
     input_list= [   ['WYS_SALAREUNION_RECTA6PERSONAS',              1, 3, 4.05, 1],
                     ['WYS_SALAREUNION_DIRECTORIO10PERSONAS',        1, 4, 6.05, 1],
                     ['WYS_SALAREUNION_DIRECTORIO20PERSONAS',        1, 5.4, 6, 1],
-                    ['WYS_PUESTOTRABAJO_CELL3PERSONAS',             5, 3.37, 3.37, 2],
+                    ['WYS_PUESTOTRABAJO_CELL3PERSONAS',             15, 3.37, 3.37, 2],
                     #['WYS_PUESTOTRABAJO_RECTO2PERSONAS',            2, 3.82, 1.4],
-                    ['WYS_PRIVADO_1PERSONA',                        0, 3.5, 2.8, 3],
-                    ['WYS_PRIVADO_1PERSONAESTAR',                   0, 6.4, 2.9, 3],
+                    ['WYS_PRIVADO_1PERSONA',                        1, 3.5, 2.8, 3],
+                    ['WYS_PRIVADO_1PERSONAESTAR',                   1, 6.4, 2.9, 3],
                     ['WYS_SOPORTE_BAÑOBATERIAFEMENINO3PERSONAS',    1, 3.54, 3.02, 4],
                     ['WYS_SOPORTE_BAÑOBATERIAMASCULINO3PERSONAS',   1, 3.54, 3.02, 4],
                     ['WYS_SOPORTE_KITCHENETTE',                     1, 1.6, 2.3, 4],
                     ['WYS_SOPORTE_SERVIDOR1BASTIDOR',               1, 1.5, 2.4, 4],
-                    ['WYS_SOPORTE_PRINT1',                          0, 1.5, 1.3, 4],
-                    ['WYS_RECEPCION_1PERSONA',                      1, 2.7, 3.25, 5],
-                    ['WYS_TRABAJOINDIVIDUAL_QUIETROOM2PERSONAS',    0, 2.05, 1.9, 6],
-                    ['WYS_TRABAJOINDIVIDUAL_PHONEBOOTH1PERSONA',    0, 2.05, 2.01, 6],
+                    ['WYS_SOPORTE_PRINT1',                          1, 1.5, 1.3, 4],
+                    ['WYS_RECEPCION_1PERSONA',                      2, 2.7, 3.25, 5],
+                    ['WYS_TRABAJOINDIVIDUAL_QUIETROOM2PERSONAS',    1, 2.05, 1.9, 6],
+                    ['WYS_TRABAJOINDIVIDUAL_PHONEBOOTH1PERSONA',    1, 2.05, 2.01, 6],
                     ['WYS_COLABORATIVO_BARRA6PERSONAS',             0, 1.95, 2.4, 6]]
     voids = []
 
