9f44d67b132ff1638507de21fcfb2aefa6a09df6
Rodrigo Hernández <rjhernandr@gmail.com>
2021-07-05 17:52:55 -0400

    viewer y async comentados

--- Layout_App/SmartLayout.py
+++ Layout_App/SmartLayout.py
@@ -5,7 +5,7 @@ from shapely.geometry import Point
 from shapely.geometry import box
 from shapely.geometry.polygon import Polygon
 from shapely.ops import unary_union
-import viewer
+#import viewer
 
 random.seed(100)
 
